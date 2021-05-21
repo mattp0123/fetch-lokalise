07ad8c0ec62f614fb3b65335ef9d042bc9aded37
Matt <mattpeng0123@gmail.com>
2021-05-21 16:10:02 +0800

    fix: fix path bug

--- index.ts
+++ index.ts
@@ -10,73 +10,75 @@ type Params = {
   outDir: string;
 };
 
-export default function fetchLokalise({ projectId, token, outDir }: Params) {
+export default async function fetchLokalise({
+  projectId,
+  token,
+  outDir,
+}: Params) {
   if (outDir === undefined || projectId === undefined || token === undefined) {
-    throw 'Params not provided';
+    throw new Error('Params not provided');
   }
 
   const bundleStructure = '%LANG_ISO%.%FORMAT%';
-  const zipFilePath = path.resolve(__dirname, 'files.zip');
+  const zipFilePath = path.resolve(outDir, 'files.zip');
   const targetPath = outDir;
 
-  (async () => {
-    console.log('fetching translations...');
-    const lokaliseApi = new LokaliseApi({ apiKey: token });
+  console.log('fetching translations...');
+  const lokaliseApi = new LokaliseApi({ apiKey: token });
 
-    const { bundle_url: downloadUrl } = await lokaliseApi.files.download(
-      projectId,
-      {
-        format: 'json',
-        original_filenames: false,
-        bundle_structure: bundleStructure,
-        // filter_langs,
+  const { bundle_url: downloadUrl } = await lokaliseApi.files.download(
+    projectId,
+    {
+      format: 'json',
+      original_filenames: false,
+      bundle_structure: bundleStructure,
+      // filter_langs,
+    }
+  );
+
+  const { data } = await axios({
+    method: 'GET',
+    url: downloadUrl,
+    responseType: 'arraybuffer',
+  });
+
+  writeFileSync(zipFilePath, data);
+  mkdirSync(targetPath, { recursive: true });
+
+  await new Promise((resolve) => {
+    yauzl.open(zipFilePath, (error, zipFile) => {
+      if (error) {
+        throw error;
       }
-    );
 
-    const { data } = await axios({
-      method: 'GET',
-      url: downloadUrl,
-      responseType: 'arraybuffer',
-    });
+      if (zipFile === undefined) {
+        throw new Error('empty zip');
+      }
 
-    writeFileSync(zipFilePath, data);
-    mkdirSync(targetPath, { recursive: true });
+      zipFile.on('entry', (entry: yauzl.Entry) => {
+        zipFile.openReadStream(entry, (error, stream) => {
+          if (error) {
+            throw error;
+          }
 
-    await new Promise((resolve) => {
-      yauzl.open(zipFilePath, (error, zipFile) => {
-        if (error) {
-          throw error;
-        }
+          if (entry.fileName === './') {
+            return;
+          }
 
-        if (zipFile === undefined) {
-          throw new Error('empty zip');
-        }
+          if (stream === undefined) {
+            return;
+          }
 
-        zipFile.on('entry', (entry: yauzl.Entry) => {
-          zipFile.openReadStream(entry, (error, stream) => {
-            if (error) {
-              throw error;
-            }
-
-            if (entry.fileName === './') {
-              return;
-            }
-
-            if (stream === undefined) {
-              return;
-            }
-
-            stream.pipe(
-              createWriteStream(path.resolve(targetPath, entry.fileName))
-            );
-          });
+          stream.pipe(
+            createWriteStream(path.resolve(targetPath, entry.fileName))
+          );
         });
-
-        zipFile.on('end', resolve);
       });
-    });
 
-    unlinkSync(zipFilePath);
-    console.log('done.');
-  })();
+      zipFile.on('end', resolve);
+    });
+  });
+
+  unlinkSync(zipFilePath);
+  console.log('done.');
 }
